343ae138880ec216847944213915af5e6f653085
Owen Medina <owensmedi@gmail.com>
2018-05-07 22:42:05 +0800

    fixed old methods and added select methods

--- CASdb.java
+++ CASdb.java
@@ -1,10 +1,23 @@
 import java.sql.*;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.*;
 
 public class CASdb {
     private static Connection con;
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-            con = getConnection(); 
+            con = getConnection();
+            LocalDate ld = LocalDate.of( 2026 , 1 , 23 );
+//            createOrderUnit();
+//            
+//            insertOrders(1, ld);
+//            insertOrderItem(1, 1);
+//            insertOrderItem(1, 4);
+//            insertOrderItem(1, 5);
+            
+            getOrder(1);
 	}
 
 	/*constructor
@@ -12,27 +25,29 @@ public class CASdb {
 		con = getConnection();
 	}*/
 	
+	//confirmed
 	public static Connection getConnection() throws Exception{ //connect to mysql db
 		try {
 			String driver = "com.mysql.cj.jdbc.Driver";
-			String url = "jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12235385"; // "//localhost OR ip add/port/dbname" //where the db is located
+			String url = "jdbc:mysql://localhost:3306/CASdb"; // "//localhost OR ip add/port/dbname" //where the db is located
 			Class.forName(driver);
 			
 			//establish connection
-			String user = "sql12235385" ;
-			String pw = "sRYC1GXARy" ;
+			String user = "root" ;
+			String pw = "" ;
 			Connection conn = DriverManager.getConnection(url, user, pw);
 			System.out.println("Connected successfully."); //tester
 			return conn;
 		}catch(Exception e) {
-			System.out.println("Error in getConnection"); //in case of any errors
+			System.out.println("Error in getConnection: " + e); //in case of any errors
 		}
 		return null;
 	}
 	
-	public static void insertOrders(int idNo, Date orderDate) throws Exception{ //insert to Order table
+	//confirmed
+	public static void insertOrders(int idNo, LocalDate ld) throws Exception{ //insert to Order table
 		try{
-			PreparedStatement newOrder = con.prepareStatement("INSERT INTO orders(idNo,orderDate) VALUES('" + idNo + "', '" + orderDate + "')" );
+			PreparedStatement newOrder = con.prepareStatement("INSERT INTO orders(idNo,orderDate) VALUES('" + idNo + "', '" + ld + "')" );
 			newOrder.executeUpdate(); //execute the insert
 		}catch(Exception e) {
 			System.out.println("Error in insertOrders " + e); //in case of any errors;
@@ -42,6 +57,7 @@ public class CASdb {
 		}
 	}
 
+	//confirmed
 	public static void insertOrderItem(int orderNo, int productNo) throws Exception{ //insert to Order Item table
 		try{
 			PreparedStatement newOrderItem = con.prepareStatement("INSERT INTO orderItem(orderNo,productNo) VALUES('" + orderNo + "', '" + productNo + "')" );
@@ -54,7 +70,8 @@ public class CASdb {
 		}
 	}
 	
-	public static void insertProduct(String name, String station, double price, String remarks, Date date) throws Exception{ //insert to Product table
+	//confirmed
+	public static void insertProduct(String name, String station, double price, String remarks, LocalDate date) throws Exception{ //insert to Product table
 		try {
 			PreparedStatement newProd = con.prepareStatement("INSERT INTO product(productName, station, salesPrice, remarks, dateSold) VALUES('" + name + "', '" + station + "', " + price + ", '" + remarks + "', '" + date + "')" );
 			newProd.executeUpdate(); //execute the insert
@@ -66,9 +83,10 @@ public class CASdb {
 		}
 	}
 	
+	//fixed, confirmed
 	public static void insertCustomer(String fName, String mName, String lName, double bal, double accDebt) throws Exception{ //insert to Customer table
 		try {
-			PreparedStatement newCustomer = con.prepareStatement("INSERT INTO customer(firstName, midName, lastName, balance, accumulatedDebt) VALUES('" + fName + "', '" + mName + "', " + lName + ", '" + bal + "', '" + accDebt + "')" );
+			PreparedStatement newCustomer = con.prepareStatement("INSERT INTO customer(firstName, midName, lastName, balance, accumulatedDebt) VALUES('" + fName + "', '" + mName + "', '" + lName + "', " + bal + ", " + accDebt + ")" );
 			newCustomer.executeUpdate(); //execute the insert
 		}catch(Exception e) {
 			System.out.println("Error in insertCustomer " + e); //in case of any errors;
@@ -78,9 +96,10 @@ public class CASdb {
 		}
 	}
 	
-	public static void insertBegInv(int prodNo,int begAmt,int totAmt,int plusAmt, int previousAmt) throws Exception{ //insert to Beginning Inventory table
+	//fixed, confirmed
+	public static void insertBegInv(LocalDate date, int prodNo,int begAmt,int totAmt,int plusAmt, int previousAmt) throws Exception{ //insert to Beginning Inventory table
 		try {
-			PreparedStatement newBegInv = con.prepareStatement("INSERT INTO beginInv(productNo, beginAmt, totalAmt, addAmt, prevAmt) VALUES('" + prodNo + "', '" + begAmt + "', '" + totAmt + "', " + int plusAmt + ", '" + previousAmt + "')" );
+			PreparedStatement newBegInv = con.prepareStatement("INSERT INTO beginInv VALUES('" + date + "', " + prodNo + ", " + begAmt + ", " + totAmt + ", " + plusAmt + ", " + previousAmt + ")" );
 			newBegInv.executeUpdate();
 		}catch(Exception e) {
 			System.out.println("Error in insertBegInv" + e);
@@ -90,10 +109,10 @@ public class CASdb {
 		}
 	}
 	
-
-public static void insertEndInv(int prodNo, int totAmt) throws Exception{  //insert to Ending Inventory table
+	//fixed, confirmed
+	public static void insertEndInv(LocalDate date, int prodNo, int totAmt) throws Exception{  //insert to Ending Inventory table
 		try {
-			PreparedStatement newEndInv = con.prepareStatement("INSERT INTO endInv(productNo, totalAmt) VALUES('" + prodNo + "', '" + totAmt + "')" );
+			PreparedStatement newEndInv = con.prepareStatement("INSERT INTO endInv VALUES('" + date + "', " + prodNo + ", " + totAmt + ")" );
 			newEndInv.executeUpdate();
 		}catch(Exception e) {
 			System.out.println("Error in insertEndInv" + e);
@@ -102,8 +121,101 @@ public static void insertEndInv(int prodNo, int totAmt) throws Exception{  //ins
 			System.out.println("Insert to EndInv completed"); 
 		}
 	}
-        
-        public static void createInventoryUnit() throws Exception{
+	
+	//confirmed
+	public static ArrayList<String> getOrder(int orderNo) throws Exception{
+		try{
+			Connection con = getConnection();
+			PreparedStatement command = con.prepareStatement("SELECT o.orderDate, cust.firstName, cust.lastName, prod.productName, oi.quantity FROM product prod, customer cust, orders o, orderItem oi WHERE o.idNo = cust.idNo AND oi.orderNo = o.orderNo AND oi.productNo = prod.productNo AND o.orderNo = " + orderNo);
+
+			ResultSet result = command.executeQuery();
+
+			ArrayList<String> results = new ArrayList<String>();
+
+			while(result.next()){
+				System.out.println(result.getString("o.orderDate") + " " + result.getString("cust.firstName") + " " + result.getString("cust.lastName") + " " + result.getString("prod.productName") + " " + result.getString("oi.quantity"));
+				results.add(result.getString("o.orderDate"));
+				results.add(result.getString("cust.firstName"));
+				results.add(result.getString("cust.lastName"));
+				results.add(result.getString("prod.productName"));
+				results.add(result.getString("oi.quantity"));
+			}
+			return results;
+		}catch(Exception e){
+			System.out.println("Error in getOrder: " + e);
+		}
+		return null;
+	}
+	
+	//confirmed
+	public static ArrayList<String> getPrice(int prodNo) throws Exception{
+		try{
+			Connection con = getConnection();
+			PreparedStatement command = con.prepareStatement("SELECT productName, salesPrice FROM product WHERE productNo = " + prodNo);
+
+			ResultSet result = command.executeQuery();
+
+			ArrayList<String> results = new ArrayList<String>();
+
+			while(result.next()){
+				System.out.println(result.getString("productName") + " " + result.getString("salesPrice"));
+				results.add(result.getString("salesPrice"));
+			}
+			return results;
+		}catch(Exception e){
+			System.out.println("Error in getPrice: " + e);
+		}
+		return null;
+	}
+	
+	//confirmed
+	public static ArrayList<String> getCustInfo(int id) throws Exception{
+		try{
+			String col1 = "balance";
+			String col2 = "accumulatedDebt";
+			Connection con = getConnection();
+			PreparedStatement command = con.prepareStatement("SELECT " + col1 + ", " + col2 + " FROM customer WHERE idNo = " + id);
+
+			ResultSet result = command.executeQuery();
+
+			ArrayList<String> results = new ArrayList<String>();
+
+			while(result.next()){
+				System.out.println(result.getString(col1) + " " + result.getString(col2));
+				results.add(result.getString(col1));
+				results.add(result.getString(col2));
+			}
+			return results;
+		}catch(Exception e){
+			System.out.println("Error in getCustInfo: " + e);
+		}
+		return null;
+	}
+	
+	//fixed, confirmed
+	public static ArrayList<String> getInv(LocalDate date, int prodNo) throws Exception{
+		try{
+			Connection con = getConnection();
+			PreparedStatement command = con.prepareStatement("SELECT beg.totalAmt, end.totalAmt FROM beginInv beg, endInv end WHERE beg.productNo = " + prodNo + " AND end.productNo = " + prodNo + " AND beg.bInvDate = '" + date + "' AND end.eInvDate = '" + date + "'");
+
+			ResultSet result = command.executeQuery();
+
+			ArrayList<String> results = new ArrayList<String>();
+
+			while(result.next()){
+				System.out.println(result.getString("beg.totalAmt") + " " + result.getString("end.totalAmt"));
+				results.add(result.getString("beg.totalAmt"));
+				results.add(result.getString("end.totalAmt"));
+			}
+			return results;
+		}catch(Exception e){
+			System.out.println("Error in getInv: " + e);
+		}
+		return null;
+	}
+    
+	//confirmed
+	public static void createInventoryUnit() throws Exception{
 		try {
 			PreparedStatement createProd = con.prepareStatement("CREATE TABLE IF NOT EXISTS product (\n" + 
 					"	productNo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,\n" + 
@@ -139,8 +251,9 @@ public static void insertEndInv(int prodNo, int totAmt) throws Exception{  //ins
 			System.out.println("Inventory unit tables created.");
 		}
 	}
-        
-        public static void createCustomerUnit() throws Exception{
+    
+	//confirmed
+	public static void createCustomerUnit() throws Exception{
 		try {
 			PreparedStatement createCustomer = con.prepareStatement("CREATE TABLE IF NOT EXISTS customer(\n" +
                                         "	idNo INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,\n" +
@@ -158,8 +271,9 @@ public static void insertEndInv(int prodNo, int totAmt) throws Exception{  //ins
 			System.out.println("Customer Table created"); //tester;
 		}
 	}
-        
-        public static void createOrderUnit() throws Exception{
+    
+	//fixed, confirmed
+	public static void createOrderUnit() throws Exception{
 		try {
 			PreparedStatement createOrders = con.prepareStatement("CREATE TABLE IF NOT EXISTS orders(\n" +
                                         "	orderNo INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,\n" +
@@ -170,6 +284,7 @@ public static void insertEndInv(int prodNo, int totAmt) throws Exception{  //ins
 			PreparedStatement createOrderItem = con.prepareStatement("CREATE TABLE IF NOT EXISTS orderItem(\n" +
                                         "	orderNo INT,\n" +
                                         "	productNo INT,\n" +
+                                        "	quantity INT,\n" +
                                         "	PRIMARY KEY( orderNo, productNo ),\n" +
                                         "	FOREIGN KEY(orderNo) REFERENCES orders(orderNo),\n" +
                                         "	FOREIGN KEY(productNo) REFERENCES product(productNo)\n" +
@@ -184,5 +299,6 @@ public static void insertEndInv(int prodNo, int totAmt) throws Exception{  //ins
 			System.out.println("Order unit tables created.");
 		}
 	}
+	
         
 }
