167d25b6f4f9b2e242e1c3d16c7ffd28a8f12f7f
Owen Medina <owensmedi@gmail.com>
2018-05-13 22:17:03 +0800

    fixed getter methods (made it void and user-friendly), fixed inventory tables, fixed update 'LIKE' to '=' - earlier commit was the wrong file

--- CASdb.java
+++ CASdb.java
@@ -9,11 +9,9 @@ public class CASdb {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
             con = getConnection();
-            LocalDate ld = LocalDate.of( 2026 , 1 , 23 );
-//            insertProduct("Pancit Bihon", "cooked food", 35.00, "1 cup per order");
-//            insertProduct("Adobong Manok", "cooked food", 50.00, "3 pieces of meat per order");
-//            insertCustomer("Jeff", "T.", "Andawi", 1500.00, 0.00);
-//            insertCustomer("Owen", "S.", "Medina", 1250.00, 0.00);
+            //createCustomerUnit();
+            //createOrderUnit();
+            //createInventoryUnit();
 	}
 
 	/*constructor
@@ -41,8 +39,9 @@ public class CASdb {
 	}
 	
 	//fixed 5/13/2018
-	public static void insertOrder(String fName, String mName, String lName, LocalDate ld) throws Exception{ //insert to Order table
+	public static void insertOrder(String fName, String mName, String lName, int year, int month, int day) throws Exception{ //insert to Order table
 		try{
+			LocalDate ld = LocalDate.of(year, month, day);
 			PreparedStatement newOrder = con.prepareStatement("INSERT INTO orders(idNo,orderDate) VALUES(" + "(SELECT idNo FROM customer WHERE firstName = '" + fName + "' AND midName = '" + mName + "' AND lastName = '" + lName + "')" + ", '" + ld + "')" );
 			newOrder.executeUpdate(); //execute the insert
 		}catch(Exception e) {
@@ -54,9 +53,9 @@ public class CASdb {
 	}
 
 	//fixed 5/13/2018
-	public static void insertOrderItem(String fName, String mName, String lName, String productName) throws Exception{ //insert to Order Item table
+	public static void insertOrderItem(String fName, String mName, String lName, String productName, int quantity) throws Exception{ //insert to Order Item table
 		try{
-			PreparedStatement newOrderItem = con.prepareStatement("INSERT INTO orderItem(orderNo,productNo) VALUES(" + "(SELECT orders.orderNo FROM orders, customer WHERE orders.idNo = customer.idNo AND customer.firstName = '" + fName + "' AND customer.midName = '" + mName + "' AND customer.lastName = '" + lName + "' ORDER BY orders.orderNo DESC LIMIT 1)" + ", " + "(SELECT productNo FROM product WHERE productName = '" + productName + "' )" + ")" );
+			PreparedStatement newOrderItem = con.prepareStatement("INSERT INTO orderItem(orderNo,productNo,quantity) VALUES(" + "(SELECT orders.orderNo FROM orders, customer WHERE orders.idNo = customer.idNo AND customer.firstName = '" + fName + "' AND customer.midName = '" + mName + "' AND customer.lastName = '" + lName + "' ORDER BY orders.orderNo DESC LIMIT 1)" + ", " + "(SELECT productNo FROM product WHERE productName = '" + productName + "' ), " + quantity + ")" );
 			newOrderItem.executeUpdate(); //execute the insert
 		}catch(Exception e) {
 			System.out.println("Error in insertOrderItem " + e); //in case of any errors;
@@ -93,9 +92,10 @@ public class CASdb {
 	}
 	
 	//fixed 5/13/2018
-	public static void insertBegInv(LocalDate date, String prodName,int begAmt, int plusAmt, int prevAmt) throws Exception{ //insert to Beginning Inventory table
+	public static void insertBegInv(int year, int month, int day, String prodName,int begAmt, int plusAmt, int prevAmt) throws Exception{ //insert to Beginning Inventory table
 		try {
-			PreparedStatement newBegInv = con.prepareStatement("INSERT INTO beginInv VALUES('" + date + "', " + "(SELECT productNo FROM product WHERE productName = '" + prodName + "')" + ", " + begAmt + ", " + (begAmt+plusAmt+prevAmt) + ", " + plusAmt + ", " + prevAmt + ")" );
+			LocalDate ld = LocalDate.of(year, month, day);
+			PreparedStatement newBegInv = con.prepareStatement("INSERT INTO beginInv VALUES('" + ld + "', " + "(SELECT productNo FROM product WHERE productName = '" + prodName + "')" + ", " + begAmt + ", " + (begAmt+plusAmt+prevAmt) + ", " + plusAmt + ", " + prevAmt + ")" );
 			newBegInv.executeUpdate();
 		}catch(Exception e) {
 			System.out.println("Error in insertBegInv" + e);
@@ -106,9 +106,10 @@ public class CASdb {
 	}
 	
 	//fixed, confirmed
-	public static void insertEndInv(LocalDate date, int prodNo, int totAmt) throws Exception{  //insert to Ending Inventory table
+	public static void insertEndInv(int year, int month, int day, String prodName, int totAmt) throws Exception{  //insert to Ending Inventory table
 		try {
-			PreparedStatement newEndInv = con.prepareStatement("INSERT INTO endInv VALUES('" + date + "', " + prodNo + ", " + totAmt + ")" );
+			LocalDate ld = LocalDate.of(year, month, day);
+			PreparedStatement newEndInv = con.prepareStatement("INSERT INTO endInv VALUES('" + ld + "', " + "(SELECT productNo FROM product WHERE productName = '" + prodName + "')" + ", " + totAmt + ")" );
 			newEndInv.executeUpdate();
 		}catch(Exception e) {
 			System.out.println("Error in insertEndInv" + e);
@@ -118,125 +119,94 @@ public class CASdb {
 		}
 	}
 	
-	//confirmed
-	public static ArrayList<String> getOrder(int orderNo) throws Exception{
+	//fixed 5/13/2018
+	public static void getOrder(int year, int month, int day, String fName, String mName, String lName) throws Exception{
 		try{
+			LocalDate ld = LocalDate.of(year, month, day);
 			Connection con = getConnection();
-			PreparedStatement command = con.prepareStatement("SELECT o.orderDate, cust.firstName, cust.lastName, prod.productName, oi.quantity FROM product prod, customer cust, orders o, orderItem oi WHERE o.idNo = cust.idNo AND oi.orderNo = o.orderNo AND oi.productNo = prod.productNo AND o.orderNo = " + orderNo);
+			PreparedStatement command = con.prepareStatement("SELECT o.orderDate, cust.firstName, cust.lastName, prod.productName, oi.quantity FROM product prod, customer cust, orders o, orderItem oi WHERE o.idNo = cust.idNo AND oi.orderNo = o.orderNo AND oi.productNo = prod.productNo AND o.orderDate = '" + ld + "' AND cust.firstName = '" + fName + "' AND cust.midName = '" + mName + "' AND cust.lastName = '" + lName + "'");
 
 			ResultSet result = command.executeQuery();
-
-			ArrayList<String> results = new ArrayList<String>();
 
 			while(result.next()){
 				System.out.println(result.getString("o.orderDate") + " " + result.getString("cust.firstName") + " " + result.getString("cust.lastName") + " " + result.getString("prod.productName") + " " + result.getString("oi.quantity"));
-				results.add(result.getString("o.orderDate"));
-				results.add(result.getString("cust.firstName"));
-				results.add(result.getString("cust.lastName"));
-				results.add(result.getString("prod.productName"));
-				results.add(result.getString("oi.quantity"));
 			}
-			return results;
 		}catch(Exception e){
 			System.out.println("Error in getOrder: " + e);
 		}
-		return null;
 	}
 	
-	//confirmed
-	public static ArrayList<String> getPrice(int prodNo) throws Exception{
+	//fixed 5/13/2018
+	public static void getPrice(String prodName) throws Exception{
 		try{
 			Connection con = getConnection();
-			PreparedStatement command = con.prepareStatement("SELECT productName, salesPrice FROM product WHERE productNo = " + prodNo);
+			PreparedStatement command = con.prepareStatement("SELECT productName, salesPrice FROM product WHERE productName = '" + prodName + "'");
 
 			ResultSet result = command.executeQuery();
-
-			ArrayList<String> results = new ArrayList<String>();
 
 			while(result.next()){
 				System.out.println(result.getString("productName") + " " + result.getString("salesPrice"));
-				results.add(result.getString("salesPrice"));
 			}
-			return results;
 		}catch(Exception e){
 			System.out.println("Error in getPrice: " + e);
 		}
-		return null;
 	}
 	
-	//confirmed
-	public static ArrayList<String> getCustInfo(int id) throws Exception{
+	//fixed 5/13/2018
+	public static void getCustInfo(String fName, String mName, String lName) throws Exception{
 		try{
-			String col1 = "balance";
-			String col2 = "accumulatedDebt";
 			Connection con = getConnection();
-			PreparedStatement command = con.prepareStatement("SELECT " + col1 + ", " + col2 + " FROM customer WHERE idNo = " + id);
+			PreparedStatement command = con.prepareStatement("SELECT idNo, CONCAT(firstName,' ', midName, ' ', lastName), balance, accumulatedDebt FROM customer WHERE firstName = '" + fName + "' AND midName = '" + mName + "' AND lastName = '" + lName + "'");
 
 			ResultSet result = command.executeQuery();
 
-			ArrayList<String> results = new ArrayList<String>();
-
 			while(result.next()){
-				System.out.println(result.getString(col1) + " " + result.getString(col2));
-				results.add(result.getString(col1));
-				results.add(result.getString(col2));
+				System.out.println(result.getString(1) + " " + result.getString(2) + " " + result.getString("balance") + " " + result.getString("accumulatedDebt"));
 			}
-			return results;
 		}catch(Exception e){
 			System.out.println("Error in getCustInfo: " + e);
 		}
-		return null;
 	}
 	
-	//fixed, confirmed
-	public static ArrayList<String> getInv(LocalDate date, int prodNo) throws Exception{
+	//fixed 5/13/2018
+	public static void getInv(int year, int month, int day, String prodName) throws Exception{
 		try{
+			LocalDate ld = LocalDate.of(year, month, day);
 			Connection con = getConnection();
-			PreparedStatement command = con.prepareStatement("SELECT beg.totalAmt, end.totalAmt FROM beginInv beg, endInv end WHERE beg.productNo = " + prodNo + " AND end.productNo = " + prodNo + " AND beg.bInvDate = '" + date + "' AND end.eInvDate = '" + date + "'");
+			PreparedStatement command = con.prepareStatement("SELECT beg.totalAmt, end.totalAmt FROM beginInv beg, endInv end, product prod WHERE beg.productNo = prod.productNo AND end.productNo = prod.productNo AND prod.productName = '" + prodName + "' AND beg.bInvDate = '" + ld + "' AND end.eInvDate = '" + ld + "'");
 
 			ResultSet result = command.executeQuery();
 
-			ArrayList<String> results = new ArrayList<String>();
-
 			while(result.next()){
-				System.out.println(result.getString("beg.totalAmt") + " " + result.getString("end.totalAmt"));
-				results.add(result.getString("beg.totalAmt"));
-				results.add(result.getString("end.totalAmt"));
+				System.out.println(prodName + " " + result.getString("beg.totalAmt") + " " + result.getString("end.totalAmt"));
 			}
-			return results;
 		}catch(Exception e){
 			System.out.println("Error in getInv: " + e);
 		}
-		return null;
 	}
     
-	//please check!
-	public static ArrayList<String> getMemWithDebt(int prodNo) throws Exception{
+	//fixed 5/13/2018
+	public static void getCustWithDebt() throws Exception{
 		try{
 			Connection con = getConnection();
-			PreparedStatement command = con.prepareStatement("SELECT idNo, CONCAT(firstname,' ', lastname),accumulatedDebt FROM customer WHERE accumulatedDebt IS NOT NULL");
+			PreparedStatement command = con.prepareStatement("SELECT CONCAT(firstName,' ', midName, ' ', lastName),accumulatedDebt FROM customer WHERE accumulatedDebt <> 0");
 
 			ResultSet result = command.executeQuery();
 
-			ArrayList<String> results = new ArrayList<String>();
-
 			while(result.next()){
-				System.out.println(result.getString("idNo") + " " + result.getString("firstname") + " " + result.getString("lastname") + " " + result.getString("accumulatedDebt"));
-				results.add(result.getString("accumulatedDebt"));
+				System.out.println(result.getString(1) + " " + result.getString("accumulatedDebt"));
 			}
-			return results;
 		}catch(Exception e){
-			System.out.println("Error in getMemWithDebt: " + e);
+			System.out.println("Error in getCustWithDebt: " + e);
 		}
-		return null;
 	}
 
 	//UPDATE METHODS
 
-	//confirmed
+	//fixed 5/13/2018
 	public static void updatePrice(String prod, double newPrice) throws Exception{
 		try {
-			PreparedStatement updatePrice = con.prepareStatement("UPDATE product SET salesPrice = " + newPrice + " WHERE productName LIKE '%" + prod + "%'");
+			PreparedStatement updatePrice = con.prepareStatement("UPDATE product SET salesPrice = " + newPrice + " WHERE productName = '" + prod + "'");
 			updatePrice.executeUpdate(); //execute the update
 		}catch(Exception e) {
 			System.out.println("Error in updatePrice" + e); //in case of any errors;
@@ -246,10 +216,10 @@ public class CASdb {
 		}
 	}
 
-	//confirmed
+	//fixed 5/13/2018
 	public static void updateStation(String prod, String newStation) throws Exception{
 		try {
-			PreparedStatement updateStation = con.prepareStatement("UPDATE product SET station ='" + newStation + "' WHERE productName LIKE '%" + prod + "%'");
+			PreparedStatement updateStation = con.prepareStatement("UPDATE product SET station ='" + newStation + "' WHERE productName = '" + prod + "'");
 			updateStation.executeUpdate(); //execute the update
 		}catch(Exception e) {
 			System.out.println("Error in updateStation" + e); //in case of any errors;
@@ -259,10 +229,10 @@ public class CASdb {
 		}
 	}
 
-	//confirmed
+	//fixed 5/13/2018
 	public static void updateRemarks(String prod, String remarks) throws Exception{
 		try {
-			PreparedStatement updateRemarks = con.prepareStatement("UPDATE product SET remarks = '" + remarks + "' WHERE productName LIKE '%" + prod + "%'");
+			PreparedStatement updateRemarks = con.prepareStatement("UPDATE product SET remarks = '" + remarks + "' WHERE productName = '" + prod + "'");
 			updateRemarks.executeUpdate(); //execute the update
 		}catch(Exception e) {
 			System.out.println("Error in updateRemarks" + e); //in case of any errors;
@@ -299,7 +269,7 @@ public class CASdb {
 	}
 
 
-	//confirmed
+	//fixed 5/13/2018
 	public static void createInventoryUnit() throws Exception{
 		try {
 			PreparedStatement createProd = con.prepareStatement("CREATE TABLE IF NOT EXISTS product (\n" + 
@@ -310,19 +280,21 @@ public class CASdb {
 					"	remarks VARCHAR(255)\n" + 
 					")");
 			PreparedStatement createBegInv = con.prepareStatement("CREATE TABLE IF NOT EXISTS beginInv (\n" + 
-					"	bInvDate DATE NOT NULL PRIMARY KEY,\n" + 
+					"	bInvDate DATE NOT NULL,\n" + 
 					"	productNo INT,\n" + 
 					"	beginAmt INT,\n" + 
 					"	totalAmt INT,\n" + 
 					"	addAmt INT,\n" + 
-					"	prevAmt INT,\n" + 
-					"	FOREIGN KEY (productNo) REFERENCES product(productNo)\n" + 
+					"	prevAmt INT,\n" +
+					"	PRIMARY KEY(bInvDate, productNo),\n" +
+					"	FOREIGN KEY(productNo) REFERENCES product(productNo)\n" + 
 					")");
 			PreparedStatement createEndInv = con.prepareStatement("CREATE TABLE IF NOT EXISTS endInv (\n" + 
-					"	eInvDate DATE NOT NULL PRIMARY KEY,\n" + 
+					"	eInvDate DATE NOT NULL,\n" + 
 					"	productNo INT,\n" + 
-					"	totalAmt INT,\n" + 
-					"	FOREIGN KEY (productNo) REFERENCES product(productNo)\n" + 
+					"	totalAmt INT,\n" +
+					"	PRIMARY KEY(eInvDate, productNo),\n" +
+					"	FOREIGN KEY(productNo) REFERENCES product(productNo)\n" + 
 					")");
 			createProd.executeUpdate();
 			createBegInv.executeUpdate();
